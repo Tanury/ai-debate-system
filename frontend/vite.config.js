a6acc8fea6aca0fe019fec8d459e35173954fb6c
Tanuri Dissanayake <142109168+Tanury@users.noreply.github.com>
2025-10-18 13:07:56 +0530

    updated configuration files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
-/**
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  publicDir: 'public',
   server: {
     port: 3000,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
@@ -18,4 +21,3 @@ export default defineConfig({
     },
   },
 })
-*/
