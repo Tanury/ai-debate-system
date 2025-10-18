c28418cac4de74eef7267536ce833e9f71a020c4
SanaliSudugodage <IT22557742@my.sliit.lk>
2025-10-18 06:48:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,7 @@ import DebateInterface from './components/DebateInterface';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <DebateInterface />
-    </div>
-  );
-}
+  
 
 export default App;
 
